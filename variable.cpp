7329ca052cb3cbb8a6a3c65a75a40bc8ede7d5f6
ml422524 <326479531@qq.com>
2016-07-01 16:59:10 +0800

    change path
    changge path.

--- variable.cpp
+++ variable.cpp
@@ -5,6 +5,6 @@ string reswpath("D:/my_matlab_files/resource");
 string restfwpath("D:/my_matlab_files/resource/txtfiles");
 string respicwpath("D:\\my_matlab_files\\resource\\pic");
 //读取资源路径
-string resrpath("D:/Documents/Visual Studio 2015/Projects/BTF/BTF");
-string restfrpath("D:/Documents/Visual Studio 2015/Projects/BTF/BTF/txtfiles");
-string respicrpath("D:\\Documents\\Visual Studio 2015\\Projects\\BTF\\BTF\\pic");
+string resrpath("C:\\Users\\Li.M\\Documents\\Visual Studio 2015\\Projects\\BTF\\BTF");
+string restfrpath("C:\\Users\\Li.M\\Documents\\Visual Studio 2015\\Projects\\BTF\\BTF\\txtfiles");
+string respicrpath("C:\\Users\\Li.M\\Documents\\Visual Studio 2015\\Projects\\BTF\\BTF\\pic");
